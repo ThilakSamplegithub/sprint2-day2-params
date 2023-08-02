8fd5c0ae9dc4cbcf393c26f74b80341c656ba681
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-08-02 13:27:37 +0530

    check did i add day2

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+app.use(express.json())
+const fs=require("fs")
+app.get("/",(req,res)=>{
+    res.send("welcome to home page")
+})
+app.get("/search",(req,res)=>{
+    const {movie}=req.query
+    if(movie){
+        res.send(`this is movie regarding ${movie}`)
+    }else{
+        res.send("please place the movie to search")
+    }
+})
+app.get("/temp",(req,res)=>{
+    const {city}=req.query
+    const data=JSON.parse(fs.readFileSync("./db.json","utf-8"))
+    console.log(data.cities[city])
+    const temperature=data.cities[city]
+    res.send(`this is ${temperature} regarding ${city}`)
+})
+app.listen(8080,()=>{
+    console.log("port is running")
+})
